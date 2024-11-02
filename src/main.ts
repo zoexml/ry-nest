c6fd5ddff32fc1ec73bb2658f1233f9b0e334696
Joet <1570466620@qq.com>
2024-11-02 18:57:31 +0800

    chore: 抽离swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { setupSwagger } from './swagger'
 import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
@@ -13,26 +13,13 @@ async function bootstrap() {
 
   // 启动热重载
   if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
+    module.hot.accept()
+    module.hot.dispose(() => app.close())
   }
   // 配置 Swagger 文档
-  const swaggerOptions = new DocumentBuilder()
-    .setTitle('Nest-后台管理API')
-    .setDescription('真正的大师永远怀着一颗学徒的心！')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build()
-
-  const document = SwaggerModule.createDocument(app, swaggerOptions)
-
-  // 项目依赖当前文档功能，最好不要改变当前地址
-  // 生产环境使用 nginx 可以将当前文档地址 屏蔽外部访问
-  SwaggerModule.setup('api-docs', app, document)
+  setupSwagger(app)
 
   await app.listen(process.env.PORT ?? 3000)
-
-  console.log('接口文档:http://localhost:3000/api-docs')
 }
 
 bootstrap()
---
+++ src/swagger.ts
@@ -0,0 +1,16 @@
+import { INestApplication } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
+export function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('Nest.js example API')
+    .setDescription('真正的大师永远怀着一颗学徒的心！')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api-docs', app, document)
+
+  console.log('接口文档:http://localhost:3000/api-docs')
+}
