2665fecae0b7ef7e579609e645f8b1f875e1d53f
Joet <1570466620@qq.com>
2024-11-02 16:17:40 +0800

    chore: 配置热重载

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+
+// import { ConfigModule } from '@nestjs/config'
+// import { TypeOrmModule } from '@nestjs/typeorm'
+// import { UserModule } from './modules/user/user.module'
+// import { AuthModule } from './modules/auth/auth.module'
+// import config from './config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  // 引入所需模块
+  imports: [
+    // 配置模块
+    // ConfigModule.forRoot({
+    //   // 全局模块
+    //   isGlobal: true,
+    //   load: [config]
+    // }),
+    // TypeOrm模块
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql', // 数据库类型
+    //   host: 'localhost', // 数据库的连接地址host
+    //   port: 3306, // 数据库的端口 3306
+    //   username: 'zoexml', // 连接账号
+    //   password: '123456', // 连接密码
+    //   database: 'nest', // 连接的表名
+    //   retryDelay: 500, // 重试连接数据库间隔
+    //   retryAttempts: 10, // 允许重连次数
+    //   synchronize: true, // 是否将实体同步到数据库
+    //   autoLoadEntities: true // 自动加载实体配置，forFeature()注册的每个实体都自己动加载
+    // }),
+    // 业务模块...
+    // UserModule,
+    // AuthModule
+  ],
+  controllers: [],
+  providers: []
 })
+
+// 这是一个类，上面是一个装饰器
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule,{ cors: true })
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true }) // 开启跨域访问
+
+  // 全局管道
+  // app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
+
+  // 启动热重载
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  // 配置 Swagger 文档
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Nest-后台管理API')
+    .setDescription('真正的大师永远怀着一颗学徒的心！')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions)
+
+  // 项目依赖当前文档功能，最好不要改变当前地址
+  // 生产环境使用 nginx 可以将当前文档地址 屏蔽外部访问
+  SwaggerModule.setup('api-docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000)
+
+  console.log('接口文档:http://localhost:3000/api-docs')
 }
+
 bootstrap()
