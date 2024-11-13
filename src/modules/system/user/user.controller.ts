d87d41967b617663262a0c031304b75827521732
Joet <1570466620@qq.com>
2024-11-13 14:46:03 +0800

    feat: user-create-profile

--- src/modules/system/user/user.controller.ts
+++ src/modules/system/user/user.controller.ts
@@ -1,47 +1,69 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common'
 import { UserService } from './user.service'
 import { CreateUserDto } from './dto'
 // import { UpdateUserDto } from './dto/update-user.dto'
-import { ApiTags, ApiOperation } from '@nestjs/swagger'
+import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger'
 
 @ApiTags('用户管理') // 给他定义一个用户的分组
 @Controller('system/user')
 export class UserController {
-//   // 使用 InjectModel 注入模型
-//   // private 定义到自己的属性上
-//   // readonly 只读的，不可被修改
+  //   // 使用 InjectModel 注入模型
+  //   // private 定义到自己的属性上
+  //   // readonly 只读的，不可被修改
   constructor(
     private readonly userService: UserService,
-//     // private readonly uploadService: UploadService,
+    // private readonly uploadService: UploadService,
   ) {}
 
+  @ApiBody({
+    type: CreateUserDto,
+    required: true,
+  })
+  //  @RequirePermission('system:user:add')
   @Post('/register')
+  // @ApiOkResponse({ type: ResponseUserDto })
   @ApiOperation({ summary: '用户-创建' })
-  create(@Body() dto: CreateUserDto) {
-    return this.userService.create(dto)
+  create(@Body() body: CreateUserDto) {
+    return this.userService.create(body)
   }
 
-//   @Get()
-//   @ApiOperation({ summary: '查找所有用户' })
-//   findAll() {
-//     return this.userService.findAll()
-//   }
+  @ApiOperation({
+    summary: '个人中心-用户信息',
+  })
+  // @RequirePermission('system:user:query')
+  @Get('/profile')
+  profile(@Request() req) {
+    const user = req.user.user
+    return this.userService.profile(user)
+  }
 
-//   @Get(':id')
-//   @ApiOperation({ summary: '根据ID查找用户' })
-//   findOne(@Param('id') id: string) {
-//     // return this.userService.findOne(id)
-//   }
+  //   create(@Body() createUserDto: CreateUserDto, @Request() req) {
+  //     createUserDto['createTime'] = GetNowDate()
+  //     createUserDto['createBy'] = req.user.user.userName
+  //     return this.userService.create(createUserDto)
+  //   }
 
-//   @Patch(':id')
-//   @ApiOperation({ summary: '更新用户' })
-//   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-//     return this.userService.update(+id, updateUserDto)
-//   }
+  //   @Get()
+  //   @ApiOperation({ summary: '查找所有用户' })
+  //   findAll() {
+  //     return this.userService.findAll()
+  //   }
 
-//   @Delete(':id')
-//   @ApiOperation({ summary: '根据id删除用户' })
-//   remove(@Param('id') id: string) {
-//     return this.userService.remove(+id)
-//   }
+  //   @Get(':id')
+  //   @ApiOperation({ summary: '根据ID查找用户' })
+  //   findOne(@Param('id') id: string) {
+  //     // return this.userService.findOne(id)
+  //   }
+
+  //   @Patch(':id')
+  //   @ApiOperation({ summary: '更新用户' })
+  //   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //     return this.userService.update(+id, updateUserDto)
+  //   }
+
+  //   @Delete(':id')
+  //   @ApiOperation({ summary: '根据id删除用户' })
+  //   remove(@Param('id') id: string) {
+  //     return this.userService.remove(+id)
+  //   }
 }
--- src/modules/system/user/user.service.ts
+++ src/modules/system/user/user.service.ts
@@ -7,60 +7,119 @@ import { CreateUserDto } from './dto'
 // // import * as bcrypt from 'bcrypt'
 // @Injectable()
 export class UserService {
-//   constructor(
-//     // 依赖注入
-//     // @InjectRepository(User)
-//     // private readonly userRepository: Repository<User>,
-//   ) {}
+  //   constructor(
+  //     // 依赖注入
+  //     // @InjectRepository(User)
+  //     // private readonly userRepository: Repository<User>,
+  //   ) {}
+
+  //     const { username, password } = dto
+  //     const userExists = await this.findOneByUserName(username)
+
+  //     if (userExists) {
+  //       throw new Error('用户名已存在')
+  //     }
+  //     // const salt = await bcrypt.genSalt()
+  //     // const hashedPassword = await bcrypt.hash(password, salt)
+  //     // dto.password = hashedPassword
+
+  //     // const user = await this.userRepository.save(dto)
+  //     // return user
+
   /**
-   * 创建用户
+   * 后台创建用户
    * @param createUserDto
    * @returns
    */
-  async create(dto: CreateUserDto) {
-//     const { username, password } = dto
-//     const userExists = await this.findOneByUserName(username)
+  async create(createUserDto: CreateUserDto) {
+    console.log('🚀 ~ UserService ~ create ~ createUserDto:', createUserDto)
+    // const loginDate = GetNowDate();
+    // const salt = bcrypt.genSaltSync(10);
+    // if (createUserDto.password) {
+    //   createUserDto.password = await bcrypt.hashSync(createUserDto.password, salt);
+    // }
 
-//     if (userExists) {
-//       throw new Error('用户名已存在')
-//     }
-//     // const salt = await bcrypt.genSalt()
-//     // const hashedPassword = await bcrypt.hash(password, salt)
-//     // dto.password = hashedPassword
+    // const res = await this.userRepo.save({ ...createUserDto, loginDate, userType: SYS_USER_TYPE.CUSTOM });
+    // const postEntity = this.sysUserWithPostEntityRep.createQueryBuilder('postEntity');
+    // const postValues = createUserDto.postIds.map((id) => {
+    //   return {
+    //     userId: res.userId,
+    //     postId: id,
+    //   };
+    // });
+    // postEntity.insert().values(postValues).execute();
 
-//     // const user = await this.userRepository.save(dto)
-//     // return user
+    // const roleEntity = this.sysUserWithRoleEntityRep.createQueryBuilder('roleEntity');
+    // const roleValues = createUserDto.roleIds.map((id) => {
+    //   return {
+    //     userId: res.userId,
+    //     roleId: id,
+    //   };
+    // });
+    // roleEntity.insert().values(roleValues).execute();
+
+    // return ResultData.ok();
   }
 
-//   findAll() {
-//     return `This action returns all user`
-//   }
+  // async create(body: SaveUserDto) {
+  //   try {
+  //     // 密码加密
+  //     const hashedPassword = await this.authService.hashPassword(body.password);
+  //     // 这里不知道为什么 prisma 客户端会报类型错误
+  //     const result = await this.prisma.user.create({
+  //       data: {
+  //         ...body,
+  //         password: hashedPassword,
+  //       } as any,
+  //     });
+  //     return responseMessage<User>(result);
+  //   } catch (error) {
+  //     // 判断是否违反 postgresql 唯一性约束
+  //     if (error.code === 'P2002') {
+  //       return responseMessage(null, '用户名、电子邮箱、手机号已存在!', -1);
+  //     }
+  //     return responseMessage(error, RESPONSE_MSG.ERROR, -1);
+  //   }
+  // }
 
-//   // async findOne(idOrUserName: string | number): Promise<User | undefined> {
-//   //   console.log('idOrUserName', idOrUserName)
+  /**
+   * 个人中心-用户信息
+   * @param user
+   * @returns
+   */
+  async profile(user) {
+    // return ResultData.ok(user);
+  }
 
-//   //   // const user = await this.userRepository.findOne({
-//   //   //   where: { idOrUserName }
-//   //   // })
+  //   findAll() {
+  //     return `This action returns all user`
+  //   }
 
-//   //   // const user =
-//   //   return
-//   // }
-//   // 根据用户名查找
-//   async findOneByUserName(username: string) {
-//     return this.userRepository.findOne({ where: { username } })
-//   }
+  //   // async findOne(idOrUserName: string | number): Promise<User | undefined> {
+  //   //   console.log('idOrUserName', idOrUserName)
 
-//   // 根据ID查找
-//   async findOneById(id: number) {
-//     return this.userRepository.findOne({ where: { id } })
-//   }
+  //   //   // const user = await this.userRepository.findOne({
+  //   //   //   where: { idOrUserName }
+  //   //   // })
 
-//   update(id: number, updateUserDto: UpdateUserDto) {
-//     return `This action updates a #${id} user`
-//   }
+  //   //   // const user =
+  //   //   return
+  //   // }
+  //   // 根据用户名查找
+  //   async findOneByUserName(username: string) {
+  //     return this.userRepository.findOne({ where: { username } })
+  //   }
 
-//   remove(id: number) {
-//     return `This action removes a #${id} user`
-//   }
+  //   // 根据ID查找
+  //   async findOneById(id: number) {
+  //     return this.userRepository.findOne({ where: { id } })
+  //   }
+
+  //   update(id: number, updateUserDto: UpdateUserDto) {
+  //     return `This action updates a #${id} user`
+  //   }
+
+  //   remove(id: number) {
+  //     return `This action removes a #${id} user`
+  //   }
 }
