87ed2c4a310910c5a8fac79f428072ff058c4579
Joet桀 <zoexml@gmail.com>
2025-11-27 11:34:30 +0800

    chore: swagger-json配置

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,5 +1,6 @@
 import { INestApplication } from '@nestjs/common'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { Response } from 'express'
 
 export function setupSwagger(app: INestApplication) {
   const options = new DocumentBuilder()
@@ -11,6 +12,11 @@ export function setupSwagger(app: INestApplication) {
 
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api-docs', app, document)
+
+  // 添加这行来提供 JSON 格式的文档访问
+  app.getHttpAdapter().get('/api-docs-json', (req, res: Response) => {
+    res.json(document)
+  })
 
   console.log('接口文档:http://localhost:3000/api-docs')
 }
